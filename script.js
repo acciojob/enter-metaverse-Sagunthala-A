cfc6343670d1971b23d5608f2b82b6fb87113404
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-16 20:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+//your JS code here. If required
+let para = document.createElement('p');
+para.setAttribute('id','status');
+para.innerText = '"Enter the Metaverse";
+
+let btn = document.createElement('button');
+btn.setAttribute('id',enterBtn);
+btn.innerText = 'Enter'
+document.body.appendChild(para,btn);
+
+
+console.log(para)
+
+
+	
