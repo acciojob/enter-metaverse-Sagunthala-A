f47c30afee43be506e4a081597ec4456ed095919
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-16 20:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 //your JS code here. If required
-let para = document.createElement('p');
-para.setAttribute('id','status');
-para.innerText = '"Enter the Metaverse";
+let para = document.createElement("p");
+para.setAttribute("id", "status");
 
-let btn = document.createElement('button');
-btn.setAttribute('id',enterBtn);
-btn.innerText = 'Enter'
-document.body.appendChild(para,btn);
+para.innerText = "Enter the Metaverse";
+
+let btn = document.createElement("button");
+btn.setAttribute("id", "enterBtn");
+btn.innerText = "Enter";
+document.body.append(para, btn);
+
+btn.addEventListener("click", () => {
+  let h = document.createElement("h1");
+  h.innerText = "Entered Metaverse";
+  para.replaceWith(h);
+});
 
 
-console.log(para)
-
-
-	
+console.log(para);
